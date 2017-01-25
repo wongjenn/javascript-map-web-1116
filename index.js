19d59da554d6cc1318e75d0a8ab3b04534f41e8d
wongjenn <wongjenn@yahoo.com>
2017-01-25 15:52:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+function map(collection, callback) {
+  const result = []
+  for (let i = 0; i < collection.length; i++){
+    const element = collection[i];
+    result.push(callback(element))
+  }
+
+  return result
+}
+
+const numbers = [1,2,3]
+const doubleNumbers = map(numbers, function(number){
+  return number * 2
+})
+
+console.log(doubleNumbers)
+
+const autobots = [
+  { name: 'Optimus Prime', strength: 5, isTransformed: false, },
+  { name: 'Ironhide', strength: 3, isTransformed: false, },
+  { name: 'Bumblebee', strength: 2.5, isTransformed: false, },
+  { name: 'Ratchet', strength: 1.5, isTransformed: false, },
+];
+
+const transformedAutobots = map(autobots, function (autobot) {
+  return Object.assign({}, autobot, {
+    strength: autobot.strength * 2,
+    isTransformed: true,
+  });
+});
+
+console.log(transformedAutobots);
